1066f4792ef241849e63c9b448de1af2c91879f9
Kalon Hinds <kalon@cloudspace.com>
2013-12-14 11:19:57 -0500

    an example of cross-filtering

--- src/app/crunchinator/crunchinator.js
+++ src/app/crunchinator/crunchinator.js
@@ -39,6 +39,40 @@ angular.module( 'ngBoilerplate.crunchinator', [
     $scope.investors = selectedItemInvestorList;
   };
 
+  $scope.updateSelectedCategory = function(item) {
+    var selectedCompanyList = [];
+    var selectedInvestorList = [];
+    console.log(item);
+    for (var i = 0; i < item.company_ids.length; i++) {
+      selectedCompanyList.push(companies[item.company_ids[i]]);
+    }
+
+    for (var j = 0; j < item.investor_ids.length; j++) {
+      selectedInvestorList.push(investors[item.investor_ids[j]]);
+    }
+
+    $scope.selectedItem = item;
+    $scope.companies = selectedCompanyList;
+    $scope.investors = selectedInvestorList;
+  };
+
+  $scope.updateSelectedInvestor = function(item) {
+    var selectedCategoryList = [];
+    var selectedCompanyList = [];
+
+    for (var i = 0; i < item.invested_company_ids.length; i++) {
+      selectedCompanyList.push(companies[item.invested_company_ids[i]]);
+    }
+
+    for (var j = 0; j < item.invested_category_ids.length; j++) {
+      selectedCategoryList.push(categories[item.invested_category_ids[j]]);
+    }
+
+    $scope.selectedItem = item;
+    $scope.categories = selectedCategoryList;
+    $scope.companies = selectedCompanyList;
+  };
+
   $http.get('/companies').success(function(response) { companies = $scope.companies = response; });
   $http.get('/categories').success(function(response) { categories = $scope.categories = response; });
   $http.get('/investors').success(function(response) { investors = $scope.investors = response; });
