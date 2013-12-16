a2a1061a37a864b4551fd92fbc21af5f1fb3ac05
Justin Ridgewell <jridgewell@cloudspace.com>
2013-12-16 14:20:00 -0500

    Cleanup Crunchinator Module

--- src/app/crunchinator/crunchinator.js
+++ src/app/crunchinator/crunchinator.js
@@ -2,7 +2,8 @@ angular.module( 'ngBoilerplate.crunchinator', [
   'ui.state',
   'ui.bootstrap',
   'plusOne',
-  'configuration'
+  'configuration',
+  'ngBoilerplate.model'
 ])
 
 .config(function config( $stateProvider ) {
@@ -18,129 +19,8 @@ angular.module( 'ngBoilerplate.crunchinator', [
   });
 })
 
-.factory('Model', ['$rootScope', '$http', function($rootScope, $http) {
-    function getModels(obj) {
-        if (!obj.prototype) { obj = obj.constructor; }
-        return obj.models || (obj.models = []);
-    }
-    function setModels(obj, models) {
-        if (!obj.prototype) { obj = obj.constructor; }
-        if (_.isArray(models)) {
-            // In case there are no ids.
-            var id = 0;
-            models = _.reduce(models, function(memo, model) {
-                memo[model.id || id++] = model;
-                return memo;
-            }, {});
-        }
-        obj.models = models;
-    }
-
-    var Model = function(attrs) {
-        var attributes = _.clone(this._attributes) || {};
-        _.extend(attributes, attrs || {});
-        this._attributes = attributes;
-        _.extend(this, attributes);
-        return this;
-    };
-
-    Model.prototype.save = function() {
-        var data = this.toObject();
-        var models = getModels(this);
-        var id = this.id;
-        if (!_.isEqual(this._attributes, data)) {
-            this._attributes = data;
-            $rootScope.$apply(function() {
-                models[id] = data;
-            });
-        }
-        return this;
-    };
-    Model.prototype.destroy = function() {
-        var models = getModels(this);
-        var id = this.id;
-        if (models[id]) {
-            $rootScope.$apply(function() {
-                delete models[id];
-            });
-        }
-        return this;
-    };
-    Model.prototype.toObject = function() {
-        var data = {};
-        _.each(_.keys(this._attributes), function(key) {
-            data[key] = this[key];
-        }, this);
-        return data;
-    };
-    Model.prototype.toJSON = function() {
-        return this.toObject();
-    };
-
-    Model.find = function(id) {
-        var Constructor = this;
-        return new Constructor(getModels(this)[id]);
-    };
-    Model.fetch = function() {
-        var _this = this;
-        var url = this.prototype.url;
-        if (!url) { throw new Error('You must specify a url on the prototype'); }
-        return $http.get(url).success(function(response) { setModels(_this, response); });
-    };
-    Model.where = function(comparator) {
-        var ms;
-        if (_.isFunction(comparator)) {
-            ms = _.select(getModels(this), comparator);
-        } else {
-            ms = _.where(getModels(this), comparator);
-        }
-        return _.map(ms, function(model) {
-            return new Model(model);
-        });
-    };
-    Model.first = function(limit) {
-        var l = limit || 1;
-        var ms = [];
-        for (var i in getModels(this)) {
-            ms.push(Model.find(i));
-            if (--l <= 0) { break; }
-        }
-        return (limit) ? ms : ms[0];
-    };
-    Model.all = function() {
-        return Model.first(Model.size());
-    };
-    Model.size = function() {
-        return _.keys(getModels(this)).length;
-    };
-
-    Model.extend = function(protoProps, staticProps) {
-        // Courtesy of Backbone.js
-        var parent = this;
-        var child;
-
-        if (protoProps && _.has(protoProps, 'constructor')) {
-            child = protoProps.constructor;
-        } else {
-            child = function() { return parent.apply(this, arguments); };
-        }
-
-        _.extend(child, parent, staticProps);
-
-        var Surrogate = function() { this.constructor = child; };
-        Surrogate.prototype = parent.prototype;
-        child.prototype = new Surrogate();
-
-        if (protoProps) { _.extend(child.prototype, protoProps); }
-
-        return child;
-    };
-
-    return Model;
-}])
-
 // CompanyModel = function(Model)
-.factory('CompanyModel', function(Model) {
+.factory('CompanyModel', ['Model', function(Model) {
     m = Model.extend({
         url: '/companies',
         _attributes: {
@@ -160,10 +40,10 @@ angular.module( 'ngBoilerplate.crunchinator', [
     });
 
     return m;
-})
+}])
 
 // CategoryModel = function(Model)
-.factory('CategoryModel', function(Model) {
+.factory('CategoryModel', ['Model', function(Model) {
     m = Model.extend({
         url: '/categories',
         _attributes: {
@@ -172,10 +52,10 @@ angular.module( 'ngBoilerplate.crunchinator', [
     });
 
     return m;
-})
+}])
 
 // CompanyModel = function(Model)
-.factory('InvestorModel', function(Model) {
+.factory('InvestorModel', ['Model', function(Model) {
     m = Model.extend({
         url: '/investors',
         _attributes: {
@@ -185,7 +65,7 @@ angular.module( 'ngBoilerplate.crunchinator', [
     });
 
     return m;
-})
+}])
 
 // CrunchinatorCtrl = function($scope) {
 .controller( 'CrunchinatorCtrl', [ '$scope', '$http', 'ENV', 'CompanyModel', 'CategoryModel', 'InvestorModel', function CrunchinatorCtrl( $scope, $http, ENV, CompanyModel, CategoryModel, InvestorModel ) {
