b8ee68d7f13331dcfe853d2bd7138f2d2b87785c
Justin Ridgewell <jridgewell@cloudspace.com>
2013-12-17 09:26:58 -0500

    Indent with two spaces

--- src/app/crunchinator/crunchinator.js
+++ src/app/crunchinator/crunchinator.js
@@ -21,50 +21,50 @@ angular.module( 'ngBoilerplate.crunchinator', [
 
 // CompanyModel = function(Model)
 .factory('CompanyModel', ['Model', function(Model) {
-    m = Model.extend({
-        url: '/companies',
-        _attributes: {
-            id: -1,
-            name: '',
-            category: '',
-            zipcode: 0,
-            funding_rounds: [],
-            // funding_rounds: {
-            // amount_raised: 0,
-            // start: new Date(),
-            // end: new Date()
-            // investors: [],
-            // },
-            total_funding: 0
-        }
-    });
+  m = Model.extend({
+    url: '/companies',
+    _attributes: {
+      id: -1,
+      name: '',
+      category: '',
+      zipcode: 0,
+      funding_rounds: [],
+      // funding_rounds: {
+      // amount_raised: 0,
+      // start: new Date(),
+      // end: new Date()
+      // investors: [],
+      // },
+      total_funding: 0
+    }
+  });
 
-    return m;
+  return m;
 }])
 
 // CategoryModel = function(Model)
 .factory('CategoryModel', ['Model', function(Model) {
-    m = Model.extend({
-        url: '/categories',
-        _attributes: {
-            name: ''
-        }
-    });
+  m = Model.extend({
+    url: '/categories',
+    _attributes: {
+      name: ''
+    }
+  });
 
-    return m;
+  return m;
 }])
 
 // CompanyModel = function(Model)
 .factory('InvestorModel', ['Model', function(Model) {
-    m = Model.extend({
-        url: '/investors',
-        _attributes: {
-            id: -1,
-            name: ''
-        }
-    });
+  m = Model.extend({
+    url: '/investors',
+    _attributes: {
+      id: -1,
+      name: ''
+    }
+  });
 
-    return m;
+  return m;
 }])
 
 // CrunchinatorCtrl = function($scope) {
