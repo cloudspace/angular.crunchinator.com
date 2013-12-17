0b5b1b11b5cd8848616d151b76cf23c7687f2b5a
Kalon Hinds <kalon@cloudspace.com>
2013-12-17 12:03:26 -0500

    the beginnings of a barchart

--- src/app/lib/angular-d3.js
+++ src/app/lib/angular-d3.js
@@ -6,11 +6,21 @@ angular.module('ngBoilerplate.crunchinator')
         data: '='
       },
       link: function(scope, element, attrs) {
-        var svg = d3.select(element[0]).append("svg").style('width', '100%');
+        var margin = 20;
+        var barHeight = 20;
+        var barPadding = 1;
+
+        var svg = d3.select(element[0])
+          .append("svg")
+          .attr('width', '100%');
         
         window.onresize = function() {
-          scope.apply();
+          scope.$apply();
         };
+
+        scope.$watch("data", function(newval, oldval) {
+          return scope.render(newval);
+        }, true);
 
         scope.$watch(function() {
           return angular.element(window)[0].innerWidth;
@@ -19,7 +29,53 @@ angular.module('ngBoilerplate.crunchinator')
         });
 
         scope.render = function(data) {
-          console.log(data);
+          svg.selectAll('*').remove();
+
+          if(!data) { return; }
+
+          var width = d3.select(element[0])[0][0].offsetWidth - margin;
+          var height = scope.data.length * (barHeight + barPadding);
+          var max = d3.max(data, function(d) { return d.count; });
+
+          svg.attr('height', height);
+          
+          svg.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', function(d, i) {
+              return i * (barHeight + barPadding);
+            })
+            .attr('y', function(d, i) {
+              var calculatedOutput = Math.round((d.count / max) * 100);
+              return !isNaN(calculatedOutput) ? height - calculatedOutput : 0;
+            })
+            .attr('width', barHeight)
+            .attr('height', function(d, i) {
+              var calculatedOutput = Math.round((d.count / max) * 100);
+              return !isNaN(calculatedOutput) ? calculatedOutput : 0;
+            })
+            .attr('fill', 'teal');
+
+          svg.selectAll('text')
+            .data(data)
+            .enter()
+            .append('text')
+            .text(function(d) {
+              return d.count;
+            })
+            .attr('text-anchor', 'middle')
+            .attr('x', function(d, i) {
+              console.log(i * (barHeight + barPadding));
+              return i * (barHeight + barPadding) + 10;
+            })
+            .attr('y', function(d, i) {
+              var calculatedOutput = Math.round((d.count / max) * 100);
+              return height - calculatedOutput + 13;
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr('fill', 'white');
         };
       }
     };
