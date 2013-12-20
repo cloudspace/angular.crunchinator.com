a7b1e3411a64886d380c8c667d3a5087b61e0a50
Justin Ridgewell <jridgewell@cloudspace.com>
2013-12-20 16:54:17 -0500

    Run tests with PhantomJS
    So Travis-CI can do our tests.

--- karma.conf.js
+++ karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
