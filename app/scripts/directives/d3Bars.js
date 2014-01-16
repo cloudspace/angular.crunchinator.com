845e4871a67b718e73770676edd6ea77a7be3b19
hkal <_@hkal.me>
2014-01-16 14:20:42 -0500

    properly display bargraph ticks

--- app/scripts/directives/d3Bars.js
+++ app/scripts/directives/d3Bars.js
@@ -7,19 +7,20 @@ angular.module('crunchinatorApp.directives').directive('d3Bars', function() {
             data: '='
         },
         link: function(scope, element) {
-            var margin = { top: 0, right: 0, bottom: 15, left: 0 };
+            var margin = { top: 0, right: 10, bottom: 20, left: 0 };
             var width = 470 - margin.left - margin.right;
             var height = 353 - margin.top - margin.bottom;
 
             var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
             var y = d3.scale.linear().range([height, 0]);
 
-            // var xAxis = d3.svg.axis().scale(x).orient('bottom');
-            // var yAxis = d3.svg.axis().scale(y).orient('left').ticks(10, '%');
+            var xAxis = d3.svg.axis().scale(x).orient('bottom');
 
             var svg = d3.select(element[0]).append('svg')
             .style('width', width + margin.left + margin.right + 'px')
             .style('height', height + margin.top + margin.bottom + 'px')
+            .style('margin', '0 auto')
+            .style('display', 'block')
             .append('g')
             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
@@ -41,46 +42,36 @@ angular.module('crunchinatorApp.directives').directive('d3Bars', function() {
                 svg.selectAll('*').remove();
                 if(!data) { return; }
 
-                x.domain(data.map(function(d) { return d.start; }));
+                var labels = _.pluck(data, 'label');
+                var labelsToDisplay = [];
+                for(var i = 0; i < labels.length; i++) {
+                    var label = labels[i];
+
+                    if(i % 2 === 0){
+                        labelsToDisplay.push(label);
+                    }
+                }
+
+                var xAxis = d3.svg.axis().scale(x).tickValues(labelsToDisplay).orient('bottom');
+
+                x.domain(data.map(function(d) { return d.label; }));
                 y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-                var hoverVal, hoverLabel;
+                svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(' + Math.floor(x.rangeBand() / 2) + ', ' + height + ')')
+                .call(xAxis);
 
                 svg.selectAll('.bar')
                 .data(data)
                 .enter().append('rect')
                 .attr('class', 'bar')
-                .attr('x', function(d) { return x(d.start); })
+                .attr('x', function(d) { return x(d.label); })
                 .attr('width', x.rangeBand())
                 .attr('y', function(d) { return y(d.count); })
                 .attr('height', function(d) { return height - y(d.count); })
-                .attr('fill', 'teal')
-                // instead of using mouseenter/mouseout we should change this to use mousemove
-                // and computationally figure out whether we're in a bar and show that value.
-                .on('mouseenter', function(d) {
-                    if (hoverVal !== undefined) { hoverVal.remove(); hoverLabel.remove(); }
-                    hoverVal = svg.append('text')
-                    .text(d.count)
-                    .attr('text-anchor', 'middle')
-                    .attr('x', x(d.start) + 17)
-                    .attr('y', y(d.count) + 20)
-                    .attr('fill', 'white');
-
-                    hoverLabel = svg.append('text')
-                    .text(d.start)
-                    .attr('text-anchor', 'middle')
-                    .attr('x', x(d.start) + 17)
-                    .attr('y', height + (margin.bottom * 0.85))
-                    .style('font-size', '9px')
-                    .attr('fill', 'teal');
-                })
-                .on('mouseout', function() {
-                    if (hoverVal !== undefined) {
-                        hoverVal.remove();
-                        hoverVal = undefined;
-                        hoverLabel.remove();
-                        hoverLabel = undefined;
-                    }
+                .on('click', function(d) {
+                    console.log(d);
                 });
             };
         }
--- app/scripts/services/componentData.js
+++ app/scripts/services/componentData.js
@@ -13,9 +13,31 @@ angular.module('crunchinatorApp.services').service('ComponentData', function() {
     this.totalFunding = _.memoize(function(companies, allCompanies) {
         if(typeof allCompanies === 'undefined' || typeof companies === 'undefined') { return; }
 
+        function abbreviateNumber(value) {
+            var newValue = value;
+            if (value >= 1000) {
+                var suffixes = ["", "K", "M", "B","T"];
+                var suffixNum = Math.floor( ((""+value).length -1)/3 );
+                var shortValue = '';
+                for (var precision = 2; precision >= 1; precision--) {
+                    shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+                    var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+                    if (dotLessShortValue.length <= 3) { break; }
+                }
+
+                newValue = shortValue+suffixes[suffixNum];
+            }
+            console.log('label name: ' + newValue);
+            return newValue;
+        }
+
+        function labelfy(num) {
+            return '$' + abbreviateNumber(num);
+        }
+
         var fundingValues = _.pluck(allCompanies, 'total_funding');
         var maxNum = parseInt(_.max(fundingValues, function(n){ return parseInt(n); }));
-        var base = 10;
+        var base = 2;
         var minGraph = 100000;
         var maxGraph = minGraph;
 
@@ -23,11 +45,11 @@ angular.module('crunchinatorApp.services').service('ComponentData', function() {
             maxGraph *= base;
         }
 
-        var ranges = [{start: 1, end: minGraph, count: 0}];
+        var ranges = [{start: 1, end: minGraph, label: labelfy(minGraph), count: 0, objs: []}];
 
         for(var i = minGraph; i < maxNum; i *= base) {
             ranges.push(
-                {start: i, end: i * base, count: 0}
+                {start: i, end: i * base, label: labelfy(i * base), count: 0, objs: []}
             );
         }
 
@@ -40,6 +62,7 @@ angular.module('crunchinatorApp.services').service('ComponentData', function() {
 
                 if (range.start < total_funding && total_funding < range.end) {
                     range.count++;
+                    range.objs.push(company);
                     break;
                 }
             }
