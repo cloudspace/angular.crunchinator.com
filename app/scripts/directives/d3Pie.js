6e9aa2616c349b8007bbb8b34958097a0b25f339
hkal <_@hkal.me>
2014-01-22 23:21:41 -0500

    modify formatting

--- app/scripts/directives/d3Pie.js
+++ app/scripts/directives/d3Pie.js
@@ -13,25 +13,25 @@ angular.module('crunchinatorApp.directives').directive('d3Pie', ['$rootScope',
             link: function(scope, element) {
                 element = angular.element(element[0]).find('.pie');
 
-            	var width = element[0].clientWidth;
-            	var height = 320;
-            	var radius = Math.min(width, height) / 2;
+                var width = element[0].clientWidth;
+                var height = 320;
+                var radius = Math.min(width, height) / 2;
                 var color = d3.scale.category20b();
 
-            	var arc = d3.svg.arc()
-            		.outerRadius(radius - 10)
-            		.innerRadius(0);
+                var arc = d3.svg.arc()
+                    .outerRadius(radius - 10)
+                    .innerRadius(0);
 
-            	var pie = d3.layout.pie()
-            		.sort(null)
-            		.value(function(d) { return d.count; });
+                var pie = d3.layout.pie()
+                    .sort(null)
+                    .value(function(d) { return d.count; });
 
-            	var svg = d3.select(element[0])
-            		.append('svg')
-            		.attr('width', width)
-            		.attr('height', height)
-            		.append('g')
-            		.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+                var svg = d3.select(element[0])
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height)
+                    .append('g')
+                    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
                 window.onresize = function() {
                     scope.$apply();
@@ -53,10 +53,10 @@ angular.module('crunchinatorApp.directives').directive('d3Pie', ['$rootScope',
                         .attr('d', arc)
                         .style('fill', function(d) { return color(d.data.label); });
 
-                    arcs.append("text")
-                        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-                        .attr("dy", ".35em")
-                        .style("text-anchor", "middle")
+                    arcs.append('text')
+                        .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+                        .attr('dy', '.35em')
+                        .style('text-anchor', 'middle')
                         .text(function(d) { return d.data.label; });
                 };
             }
