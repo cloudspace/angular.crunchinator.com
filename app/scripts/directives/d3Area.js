9569765901e4ffc5b9bf000f8bf94a405b5a0ba9
hkal <_@hkal.me>
2014-02-07 00:40:12 -0500

    appease linter

--- app/scripts/directives/d3Area.js
+++ app/scripts/directives/d3Area.js
@@ -122,18 +122,18 @@ angular.module('crunchinatorApp.directives').directive('d3Area', ['$rootScope',
                         .attr('height', height);
 
                     gBrush.selectAll('.resize').append('path').attr('d', function(d) {
-                        var e = +(d == "e"),
+                        var e = +(d === 'e'),
                             x = e ? 1 : -1,
                             y = height / 3;
-                        return "M" + (.5 * x) + "," + y
-                            + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6)
-                            + "V" + (2 * y - 6)
-                            + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y)
-                            + "Z"
-                            + "M" + (2.5 * x) + "," + (y + 8)
-                            + "V" + (2 * y - 8)
-                            + "M" + (4.5 * x) + "," + (y + 8)
-                            + "V" + (2 * y - 8);
+                        return 'M' + (0.5 * x) + ',' + y +
+                            'A6,6 0 0 ' + e + ' ' + (6.5 * x) + ',' + (y + 6) +
+                            'V' + (2 * y - 6) +
+                            'A6,6 0 0 ' + e + ' ' + (0.5 * x) + ',' + (2 * y) +
+                            'Z' +
+                            'M' + (2.5 * x) + ',' + (y + 8) +
+                            'V' + (2 * y - 8) +
+                            'M' + (4.5 * x) + ',' + (y + 8) +
+                            'V' + (2 * y - 8);
                     });
                 };
             }
