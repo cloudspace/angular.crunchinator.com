9bdfef35c09d70ce3e58335285f422f9b3c29da9
hkal <_@hkal.me>
2014-02-12 12:00:53 -0500

    fix state filtering on investors

--- app/scripts/services/investor.js
+++ app/scripts/services/investor.js
@@ -189,12 +189,13 @@ angular.module('crunchinatorApp.models').service('Investor', function(Model, API
 
             if (states.length !== 0) {
                 this.dimensions.byStates.filter(function(company_states) {
-                    if(states.length === 0) { return true; }
-
                     for(var i = 0; i < company_states.length; i++) {
                         var company_state = company_states[i];
-                        return _.contains(states, company_state);
+                        if (_.contains(states, company_state)) {
+                            return true
+                        }
                     }
+                    return false;
                 });
             }
         },
