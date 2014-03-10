7bc7d09d02d0f0ffb7fb7610d1d6df3a8f817387
Nicholas Thomson <nick@cloudspace.com>
2014-03-10 17:58:12 -0400

    Secure bitly key

--- app/scripts/services/bitly.js
+++ app/scripts/services/bitly.js
@@ -1,9 +1,8 @@
 'use strict';
 
-angular.module('crunchinatorApp.services').service('Bitly', function($http) {
+angular.module('crunchinatorApp.services').service('Bitly', ['$http', 'BITLY_KEY', function($http, BITLY_KEY) {
     this.shorten = function(url) {
-        var access_token = '98766bc61a09e3d4cd9c5d16dae3b2b604f97a98';
-        var query = 'https://api-ssl.bitly.com/v3/shorten?access_token='+access_token+'&longUrl='+url;
+        var query = 'https://api-ssl.bitly.com/v3/shorten?access_token='+BITLY_KEY+'&longUrl='+url;
 
         return $http({
             method: 'GET',
@@ -13,4 +12,4 @@ angular.module('crunchinatorApp.services').service('Bitly', function($http) {
             }
         });
     };
-});
+}]);
