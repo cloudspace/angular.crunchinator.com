e326e29523f5e0b5ba11824421007cd178b70938
hkal <_@hkal.me>
2014-03-12 13:07:25 -0400

    scroll to top on about-section navigation

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -4,10 +4,24 @@ angular.module('crunchinatorApp.controllers')
 .controller('AboutCtrl', [
     '$scope',
     function AboutCtrl($scope) {
+        var section = angular.element('#about');
+        var body = angular.element('body');
+
         $scope.showPage = 'about';
         $scope.quizPos = 0;
         $scope.incPos = function(){
             $scope.quizPos++;
+        };
+
+        $scope.navigate = function(page) {
+            if (body.scrollTop() === section.offset().top) {
+                $scope.showPage = page;
+            } else {
+                angular.element('body').animate({scrollTop: section.offset().top}, 'slow', function() {
+                    $scope.showPage = page;
+                    $scope.$digest();
+                });
+            }
         };
     }
 ]);
