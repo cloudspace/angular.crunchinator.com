bfc0aee59b0fd77e607bd0e7de16301e8b42a75b
Nicholas Thomson <nick@cloudspace.com>
2014-03-17 12:18:45 -0400

    background color and affix fixing

--- app/scripts/directives/affix.js
+++ app/scripts/directives/affix.js
@@ -34,10 +34,8 @@ angular.module( 'crunchinatorApp.directives').directive( 'affix', ['$rootScope',
                     };
                 }
 
-                $rootScope.$on('scrollFinish', function() {
-                    angular.element(element[0]).affix(config);
-                    scope.$digest();
-                });
+                angular.element(element[0]).affix(config);
+
             }
         };
     }
