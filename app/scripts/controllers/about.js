db4a6988a093bb2f7d33e69abe66f0f3b1effa67
Nicholas Thomson <nick@cloudspace.com>
2014-03-19 16:01:59 -0400

    Custom events for Analytics

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -11,6 +11,7 @@ angular.module('crunchinatorApp.controllers')
         $scope.isMobile = Browser.isMobile.any();
 
         $scope.navigate = function(page) {
+            _gaq.push(['_trackEvent', 'Navigation', 'Click', page]);
             if (body.scrollTop() === section.offset().top) {
                 $scope.showPage = page;
             } else {
