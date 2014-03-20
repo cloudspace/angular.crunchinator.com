1f4db1de2ec9cf48d7dd258d548ea0e4cb3e39f8
Nicholas Thomson <nick@cloudspace.com>
2014-03-20 15:31:56 -0400

    Kissmetrics. Analytics service

--- app/scripts/directives/emailShare.js
+++ app/scripts/directives/emailShare.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module( 'crunchinatorApp.directives').directive( 'emailShare', [
-    function () {
+angular.module( 'crunchinatorApp.directives').directive( 'emailShare', [ 'Analytics',
+    function (Analytics) {
         return {
             restrict: 'A',
             link: function (scope, element) {
                 element.bind('click', function(){
-                    ga('send', 'event', 'Social', 'Share', 'Email');
+                    Analytics.event('Social', 'Share', 'Email');
                     var shortened = encodeURIComponent(scope.$parent.shortUrl);
                     var link = 'mailto:?body='+shortened;
                     window.location.href = link;
--- app/scripts/directives/twitterShare.js
+++ app/scripts/directives/twitterShare.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module( 'crunchinatorApp.directives').directive( 'twitterShare', ['$window',
-    function ($window) {
+angular.module( 'crunchinatorApp.directives').directive( 'twitterShare', ['$window', 'Analytics',
+    function ($window, Analytics) {
         return {
             restrict: 'A',
             link: function (scope, element) {
                 element.on('click', function(){
-                    ga('send', 'event', 'Social', 'Share', 'Twitter');
+                    Analytics.event('Social', 'Share', 'Twitter');
                     var shortened = encodeURIComponent(scope.$parent.shortUrl);
                     var url = 'https://twitter.com/share?url='+shortened;
                     $window.open(url, 'Twitter', 'width=575,height=400');
